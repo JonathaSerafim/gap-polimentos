440733c0bbd963c9edf67e1fc6f8401870716553
JonathaSerafim <seraphimjonatha@gmail.com>
2023-05-23 18:37:22 -0300

    Só pq sou burro

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const FooterComponent = () => {
-<footer className={styles["footer"]}>
+<footer className={styles["foot"]}>
 <button className={styles["number"]} ><a href="https://wa.me/5511972066568">Contato: <strong>(11) 97206-6568</strong></a></button>
 </footer>
 }
